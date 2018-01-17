4b831eba1a0fd4d38ba5eb2647319e09dd10c9b0
Bas Pennings <baspster@gmail.com>
2018-01-17 21:47:38 +0100

    Fast bitmap operations for .NET

---
+++ Pixlr/BitmapExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace Pixlr
+{
+    using System;
+    using System.Drawing;
+
+    public class Unsafe
+    {
+        private readonly Bitmap src;
+
+        public Unsafe(Bitmap src)
+        {
+            this.src = src;
+        }
+
+        public void MapInPlace(Func<Color, Color> f)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public static class BitmapExtensions
+    {
+        public static Unsafe Unsafe(this Bitmap self) => new Unsafe(self);
+    }
+}
